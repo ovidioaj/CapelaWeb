7c17c8be40233ef762c1b2c36cd53dfa559b70a7
diegogarciaa <diego.fismat@gmail.com>
2015-08-31 10:40:13 -0300

    reconfig database

--- Capela.Web/Business/LocalDatabase.cs
+++ Capela.Web/Business/LocalDatabase.cs
@@ -4,16 +4,15 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Data.Entity;
 using System.Data.SqlClient;
+
 namespace Capela.Web.Business
 {
         public class LocalDatabase: IdentityDbContext<ApplicationUser>
         {
-                private const string productionServer = @"ea5e1895-5cab-43b0-adba-a47d00e679e1.sqlserver.sequelizer.com";
-                private const string productionDatabase = @"dbea5e18955cab43b0adbaa47d00e679e1";
-                private const string stagggingServer = @"3bfc5672-d21f-44e8-a5e0-a4d401664fb4.sqlserver.sequelizer.com";
-                private const string staggingDatabase = @"db3bfc5672d21f44e8a5e0a4d401664fb4";
-                private const string userId = @"ajzcpwocfejcokoo";
-                private const string social = @"fRpJ8zT7xyCezec4QLEKY6HY5LhfMJHgqTWnzEXHJ5QmyEMhgAx735ShhxNusNtW";
+                private const string productionServer = @"dcaeb7d0-f53b-43cd-af5c-a50500de5173.sqlserver.sequelizer.com";
+                private const string productionDatabase = @"dbdcaeb7d0f53b43cdaf5ca50500de5173";
+                private const string userId = @"zzytpfmbsphvfsgb";
+                private const string social = @"nHQHvnPrVWjUTxwPCJD5SPD8n6EanwQLYGUgT8SQoP8XcQsumLvKotsFeRuyQPdy";
 
                 public DbSet<CapelaGroup> CapelaGroups { get; set; }
 
@@ -36,21 +35,21 @@ namespace Capela.Web.Business
 
                 static LocalDatabase()
                 {
-                        System.Data.Entity.Database.SetInitializer(new MigrateDatabaseToLatestVersion<LocalDatabase, Migrations.Configuration>());
+                        Database.SetInitializer(new MigrateDatabaseToLatestVersion<LocalDatabase, Migrations.Configuration>());
                 }
 
                 private static string GetConnection()
                 {
                         var connection = new SqlConnectionStringBuilder();
 
-                        connection.DataSource = stagggingServer;
-                        connection.InitialCatalog = staggingDatabase;
+                        connection.DataSource = productionServer;
+                        connection.InitialCatalog = productionDatabase;
                         connection.UserID = userId;
                         connection.Password = social;
 
                         return connection.ConnectionString;
                 }
 
-                public System.Data.Entity.DbSet<Capela.Web.Entities.CapelaEvents> CapelaEvents { get; set; }
+                public DbSet<CapelaEvents> CapelaEvents { get; set; }
         }
 }
